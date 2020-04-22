42ddd397a78c029b80f6e8e980cc8916af6beb96
jainakhil93 <jainakhil93@gmail.com>
2020-04-22 13:11:26 +1000

    Creating a Header

--- react/the_venue/src/App.js
+++ react/the_venue/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './resources/styles.css';
+import Header from './components/header_footer/Header'
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    )
+  }
 }
 
 export default App;
