929f8ff28a2844e84a195f71db9b4ae1d2395f90
jainakhil93 <jainakhil93@gmail.com>
2020-04-30 00:01:43 +1000

    Adding a slider for images

--- react/the_venue/src/App.js
+++ react/the_venue/src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import './resources/styles.css';
+import React, { Component } from "react";
+import "./resources/styles.css";
 
-import Header from './components/header_footer/Header';
-
+import Header from "./components/header_footer/Header";
+import Featured from "./components/featured/index";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
-        <Header/>
+      <div
+        className="App"
+        style={{ height: "1500px", background: "cornflowerblue" }}
+      >
+        <Header />
+        <Featured />
       </div>
     );
   }
