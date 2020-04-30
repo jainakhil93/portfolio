09742e1910e108a40d599abdf19d27def1b36682
jainakhil93 <jainakhil93@gmail.com>
2020-04-30 20:05:48 +1000

    Add Highlights section with reusable button

--- react/the_venue/src/App.js
+++ react/the_venue/src/App.js
@@ -4,17 +4,16 @@ import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured/index";
 import VenueNfo from "./components/venueNfo";
+import Highlight from "./components/Highlights";
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px", background: "cornflowerblue" }}
-      >
+      <div>
         <Header />
         <Featured />
         <VenueNfo />
+        <Highlight />
       </div>
     );
   }
