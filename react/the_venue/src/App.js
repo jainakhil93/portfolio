f99630d35b036e695ada107ab4b06e15c18703c8
jainakhil93 <jainakhil93@gmail.com>
2020-04-30 21:24:07 +1000

    Adding Footer and Map

--- react/the_venue/src/App.js
+++ react/the_venue/src/App.js
@@ -6,6 +6,8 @@ import Featured from "./components/featured/index";
 import VenueNfo from "./components/venueNfo";
 import Highlight from "./components/Highlights";
 import Pricing from "./components/pricing";
+import Location from "./components/location";
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
@@ -16,6 +18,8 @@ class App extends Component {
         <VenueNfo />
         <Highlight />
         <Pricing />
+        <Location />
+        <Footer />
       </div>
     );
   }
---
+++ react/the_venue/src/components/location/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <div className="location_wrapper">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.687634355261!2d151.22928471538853!3d-33.92343812932838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12b1884e5932bd%3A0x43bb6db0fd990ee3!2s31%20Meeks%20St%2C%20Kingsford%20NSW%202032!5e0!3m2!1sen!2sau!4v1588245350813!5m2!1sen!2sau"
+        width="100%"
+        height="500px"
+        frameBorder="0"
+        allowFullScreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
+      <div className="location_tag">
+        <div>Location</div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
