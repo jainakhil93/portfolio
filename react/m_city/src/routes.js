1452257630883983ad544661d33352dc6c7acbf2
jainakhil93 <jainakhil93@gmail.com>
2020-05-14 00:53:00 +1000

    Adding Header

---
+++ react/m_city/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './HOC/Layout'
+const Routes = (props) => {
+  return (
+    <Layout>
+      Children
+    </Layout>
+  )
+}
+
+export default Routes;
