7c09227bb3ab299b4c2361789ec6f529e1de4c12
jainakhil93 <jainakhil93@gmail.com>
2020-05-18 16:04:07 +1000

    Animating Stripes

--- react/m_city/src/routes.js
+++ react/m_city/src/routes.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Layout from './HOC/Layout'
+import Layout from './HOC/Layout';
+import { Switch, Route } from 'react-router-dom';
+import Home from "./Components/home"
 const Routes = (props) => {
   return (
     <Layout>
-      Children
+      <Switch>
+        <Route exact component={Home} path="/"/>
+      </Switch>
     </Layout>
   )
 }
