15e1293cacbf09638695ffaf04ab4e46b5b42fd3
jainakhil93 <jainakhil93@gmail.com>
2020-06-03 18:12:36 +1000

    setup matches with firebase

---
+++ react/m_city/src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app"
+import "firebase/app"
+import "firebase/database"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCBFB6LV-ueW_e8WfmaOMTnc3xrq8uJfM",
+    authDomain: "m-city-37a97.firebaseapp.com",
+    databaseURL: "https://m-city-37a97.firebaseio.com",
+    projectId: "m-city-37a97",
+    storageBucket: "m-city-37a97.appspot.com",
+    messagingSenderId: "926475138545",
+    appId: "1:926475138545:web:cf2d5801aa43f6f6eac050",
+    measurementId: "G-SH39NQVMYG"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches')
+export {
+    firebase,
+    firebaseMatches
+}
